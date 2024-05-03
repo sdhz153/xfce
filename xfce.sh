66002773606b9e1f6a37db34477e4ad9a8c74c4b
sdhz151 <yanghui140@gmail.com>
2024-05-03 22:58:31 +0800

    Add files via upload

---
+++ xfce.sh
@@ -0,0 +1,2 @@
+sudo dnf install dejavu-fonts liberation-fonts gnu-*-fonts google-*-fonts xorg-*  xfwm4 xfdesktop xfce4-* xfce4-*-plugin network-manager-applet *fonts  lightdm lightdm-gtk && echo 'user-session=xfce' >> /etc/lightdm/lightdm.conf.d/60-lightdm-gtk-greeter.conf 
+

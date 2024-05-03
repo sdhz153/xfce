5345738b518d65de3ab0357fac5f44a3497104f8
sdhz151 <yanghui140@gmail.com>
2024-05-03 23:11:13 +0800

    Add files via upload

--- xfce.sh
+++ xfce.sh
@@ -1,2 +1,2 @@
-sudo dnf install dejavu-fonts liberation-fonts gnu-*-fonts google-*-fonts xorg-*  xfwm4 xfdesktop xfce4-* xfce4-*-plugin network-manager-applet *fonts  lightdm lightdm-gtk && echo 'user-session=xfce' >> /etc/lightdm/lightdm.conf.d/60-lightdm-gtk-greeter.conf 
+dnf install dejavu-fonts liberation-fonts gnu-*-fonts google-*-fonts xorg-*  xfwm4 xfdesktop xfce4-* xfce4-*-plugin network-manager-applet *fonts  lightdm lightdm-gtk && echo 'user-session=xfce' >> /etc/lightdm/lightdm.conf.d/60-lightdm-gtk-greeter.conf && systemctl start lightdm && systemctl enable lightdm && systemctl set-default graphical.target && systemctl disable gdm && reboot
 
